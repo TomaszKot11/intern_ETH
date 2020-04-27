15071a2570e9c144ebd6641df976c335a4ac4ffe
Tomasz <kottomasz98@gmail.com>
2020-04-28 00:23:20 +0200

    Add some tests

--- polynomynal.rb
+++ polynomynal.rb
@@ -7,15 +7,15 @@ class Polynomynal
     end
     
     # This is a solution for a task in OOP manner
-    # benath class definition there is a function with two input arrays :)
+    # benath is the solution with two input files
     def add_polynomynal(polynomynal)
-        raise Error, 'The input should be a polynomynal' unless polynomynal.instance_of? Polynomynal
+        raise ArgumentError, 'The input should be a polynomynal' unless polynomynal.instance_of? Polynomynal
         # C-style - first declare variables
         poly1_arr_size = @coefficient_arr.size 
         poly2_arr_size = polynomynal.coefficient_arr.size
         smaller_arr = poly1_arr_size <= poly2_arr_size ? @coefficient_arr : polynomynal.coefficient_arr # reference types
-        # make two arrays equal (loop)
-        (poly1_arr_size - poly2_arr_size).times { smaller_arr << 0 } 
+        # make two arrays equal (loop) - push to the head of an array 0
+        (poly1_arr_size - poly2_arr_size).times { smaller_arr.unshift(0) } 
         # element wise addition (hint: the last line of Ruby's function is returned)
         Polynomynal.new([@coefficient_arr, polynomynal.coefficient_arr].transpose.map { |x| x.reduce(:+) })
     end
@@ -30,9 +30,9 @@ class Polynomynal
         coefficient_arr_size = coefficient_arr.size
 
         @coefficient_arr.each_with_index do |el, idx|
-            sign = el < 0 || idx == 0 ? '' : '+'
+            sign = el < 0 || idx == 0 ? '' : ' + '
             iks_add = coefficient_arr_size - (idx + 1) > 0 ? "x^#{coefficient_arr_size - (idx + 1)}" : ''
-            str += "#{sign}#{el}#{iks_add}"if el != 0
+            str += "#{sign}#{el.to_s.gsub('-', ' - ')}#{iks_add}" if el != 0
         end
 
         str
--- spec/polynomynal_spec.rb
+++ spec/polynomynal_spec.rb
@@ -5,14 +5,59 @@ require './polynomynal'
 RSpec.describe 'Polynomynal' do 
 
     context '#initialization' do
-        it 'should be true'
+        let(:coefficients_arr) { [-100, 123, 12, 34, 6] }
+        it 'should initialize polynomynal with provided array of coefficients' do 
+            expect(Polynomynal.new(coefficients_arr).coefficient_arr).to match_array(coefficients_arr)
+        end
     end
 
     context '#add_polynomynal' do
-        it 'should be true'
+        let(:polynomynal) { Polynomynal.new([1, 2, 5, 6]) }
+        context 'not Polynomynal instance input provided' do 
+            it 'should raise error with proper message' do 
+                expect { polynomynal.add_polynomynal('Not polynomynal object') }.to raise_error(an_instance_of(ArgumentError).and having_attributes(message: 'The input should be a polynomynal'))
+            end
+        end
+
+        context 'polynomynals with smaller degree provided' do
+            context 'positive coefficients' do 
+                let(:polynomynal_smaller_degree) { Polynomynal.new([1, 2, 3]) }
+
+                it 'should return proper polynomynal' do 
+                    # expect()
+                end
+            end
+
+            context 'negative coefficients' do 
+
+            end
+        end
+
+        context 'polynomynals with greater degree provided' do 
+            context 'positivve coefficients' do 
+
+            end
+
+            context 'negative coefficients' do 
+            
+            end
+        end
+
+        context 'polynomynals with the same degree provided' do
+            context 'positive coefficients' do 
+            end
+
+            context 'negative coefficients' do 
+            end
+        end
     end
 
     context '#to_s' do 
-        it 'should be true'
+        let(:polynomynal) { Polynomynal.new([1, 2, 1, 5, -10, 5, 1]) }
+        let(:polynomynal_string) { '1x^6 + 2x^5 + 1x^4 + 5x^3 - 10x^2 + 5x^1 + 1' }
+
+        it 'should return properly formatted polynomynal string' do 
+            expect(polynomynal.to_s).to eq(polynomynal_string)
+        end
     end
 end
